e9a1303b8c47b06e13331625fb8d361232c90c63
Ayam Dobhal <me@iamdobhal.dev>
2023-07-16 06:22:34 +0530

    model: use skops instead of joblib

--- app/helpers/emotion_analysis.py
+++ app/helpers/emotion_analysis.py
@@ -1,10 +1,15 @@
-import skops.io as sio
 import os
 
-if not os.path.exists('emotion-detection/model.skops'):
+import skops.io as sio
+
+if not os.path.exists("emotion-detection/model.skops"):
     raise Exception("Model not found")
 
-model = sio.load('emotion-detection/model.skops', trusted=True)
+model = sio.load("emotion-detection/model.skops", trusted=True)
+
 
 def get_emotion(text: str) -> str:
+    """
+    Get emotion from text
+    """
     return model.predict([text])[0]
